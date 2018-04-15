cc63b3c1004d3593bdf452f95a2b31ddb009ecbe
markonnen <jordan.walcaraz@gmail.com>
2018-04-15 10:36:06 -0400

    Android merge - css improvements with floating button
    - Added listener in android platform.js to scale-in the "add-person button" after page load, and small animation to get user's attention
    - Will try to get the same working for the "add-gift button"

--- Giftr/merges/android/js/platform.js
+++ Giftr/merges/android/js/platform.js
@@ -1,8 +1,23 @@
 app.platformConstants = {
     platform: "Android",
+
     buttons: {
         addPersonButton: document.getElementById('add-person-button-android'),
         addGiftButton: document.getElementById('add-gift-button-android'),
         giftListBackButton: document.getElementById('gift-back-button-android')
+    },
+
+    // Perform initial platform-specific actions
+    init: function(){
+        // Scale-in add person button on home screen first time app is opened, and add pulse to get user attention
+        setTimeout(() => {
+            app.platformConstants.buttons.addPersonButton.parentElement.classList.add('scale-in');
+        }, 800);
+        app.platformConstants.buttons.addPersonButton.parentElement.classList.add('pulse');
+        setTimeout(() => {
+            app.platformConstants.buttons.addPersonButton.parentElement.classList.remove('pulse');
+        }, 2000);
     }
-}
+};
+
+document.addEventListener(loadEvent, app.platformConstants.init);
--- Giftr/platforms/android/app/src/main/assets/www/js/platform.js
+++ Giftr/platforms/android/app/src/main/assets/www/js/platform.js
@@ -1,8 +1,21 @@
 app.platformConstants = {
     platform: "Android",
+
     buttons: {
         addPersonButton: document.getElementById('add-person-button-android'),
         addGiftButton: document.getElementById('add-gift-button-android'),
         giftListBackButton: document.getElementById('gift-back-button-android')
+    },
+
+    init: function(){
+        setTimeout(() => {
+            app.platformConstants.buttons.addPersonButton.parentElement.classList.add('scale-in');
+        }, 800);
+        app.platformConstants.buttons.addPersonButton.parentElement.classList.add('pulse');
+        setTimeout(() => {
+            app.platformConstants.buttons.addPersonButton.parentElement.classList.remove('pulse');
+        }, 2000);
     }
-}
+};
+
+document.addEventListener(loadEvent, app.platformConstants.init);
