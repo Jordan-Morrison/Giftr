5086255e45609ad1bd0848ffab9853b8941cce09
markonnen <jordan.walcaraz@gmail.com>
2018-04-16 11:20:13 -0400

    Android merge - Revert changes to Androidmanifest.xml
    AndroidManifest.xml
    - <activity> element attribute from android:windowSoftInputMode reverted back to "adjustResize"
    - "adjustPan" created issues where input fields were hidden by the keyboard
    - may later attempt implementation using cordova keyboard plugin

--- Giftr/platforms/android/app/build/intermediates/assets/debug/www/js/platform.js
+++ Giftr/platforms/android/app/build/intermediates/assets/debug/www/js/platform.js
@@ -7,9 +7,14 @@ app.platformConstants = {
         giftListBackButton: document.getElementById('gift-back-button-android')
     },
 
-    // Perform initial platform-specific actions
+// Perform initial platform-specific actions
     init: function(){
         // Scale-in add person button on home screen first time app is opened, and add pulse to get user attention
+        app.platformConstants.personAddAnimate();
+    },
+
+// Delay, scale-in and pulse the add person button on the person list page
+    personAddAnimate: function() {
         setTimeout(() => {
             app.platformConstants.buttons.addPersonButton.parentElement.classList.add('scale-in');
         }, 800);
@@ -18,7 +23,7 @@ app.platformConstants = {
             app.platformConstants.buttons.addPersonButton.parentElement.classList.remove('pulse');
         }, 2000);
     },
-
+// Animate add gift button on gift list page
     giftAddAnimate: function(){
         setTimeout(() => {
             app.platformConstants.buttons.addGiftButton.parentElement.classList.add('scale-in');
